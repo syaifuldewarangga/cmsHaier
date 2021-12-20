7a4a483e0677ea13152734fabe9cfaf78f8f29d5
syaiful <syaiful.dewarangga@gmail.com>
2021-12-20 17:03:51 +0700

    add customer voice report

--- src/component/dahboard/report/Report.jsx
+++ src/component/dahboard/report/Report.jsx
@@ -53,59 +53,6 @@ function Report(props) {
         tableToCSV('table-customer-voice', 'Report Customer Voice')
       })
     }
-    
-    
-  };
-
-  const JSONToCSVConvertor = (JSONData, ReportTitle, ShowLabel) => {
-    var arrData =
-      typeof JSONData !== 'object' ? JSON.parse(JSONData) : JSONData;
-
-    var CSV = '';
-
-    if (ShowLabel) {
-      var row = '';
-
-      for (var index in arrData[0]) {
-        row += index + ',';
-      }
-
-      row = row.slice(0, -1);
-
-      CSV += row + '\r\n';
-    }
-
-    for (var i = 0; i < arrData.length; i++) {
-      var row = '';
-
-      for (var index in arrData[i]) {
-        row += '"' + arrData[i][index] + '",';
-      }
-
-      row.slice(0, row.length - 1);
-
-      CSV += row + '\r\n';
-    }
-
-    if (CSV === '') {
-      alert('Invalid data');
-      return;
-    }
-
-    var fileName = 'Report_';
-    fileName += ReportTitle.replace(/ /g, '_');
-
-    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
-
-    var link = document.createElement('a');
-    link.href = uri;
-
-    link.style = 'visibility:hidden';
-    link.download = fileName + '.csv';
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
   };
 
   return (
@@ -279,6 +226,25 @@ function Report(props) {
               </Fragment>
             )
           })
+        }
+      </table>
+
+      <table className='d-none' id="table-customer-voice">
+        <tr>
+          <th>Name</th>
+          <th>Question</th>
+          <th>Answer</th>
+          <th>Created At</th>
+        </tr>
+        {
+          customerVoice.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.question}</td>
+              <td>{item.answer}</td>
+              <td>{item.created_at}</td>
+            </tr>
+          ))
         }
       </table>
     </div>
